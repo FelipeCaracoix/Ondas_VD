47999f9a57d4ce25bcdba959ee85df227485d0e4
GuidoDSalem <guidosalem.dev@gmail.com>
2024-06-25 14:09:02 -0300

    agregue el gif

--- src/App.svelte
+++ src/App.svelte
@@ -109,7 +109,7 @@
   <div class="onda">
     <iframe src="https://www.geogebra.org/calculator/ns3kjjf7?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
   </div>
-
+  <img src="/public/images/gifSumaOnda.gif" alt="miGif">
   
 
 
